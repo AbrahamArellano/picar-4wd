82946aad675cb48451d141f1198b621ba2fcbf74
AbrahamArellano <abraham.tavara@gmail.com>
2023-12-26 12:48:17 +0100

    Adjusted algorithm to scan room

--- examples/iot_scan_test.py
+++ examples/iot_scan_test.py
@@ -2,33 +2,60 @@ import picar_4wd as fc
 import random
 import time
 
-speed = 30
-back_speed = 5
+speed = 10
+back_speed = 2
 back_time = 0.2
+on_stop = False
+
+def are_all_elements_equal_to_2(arr):
+    for element in arr:
+        if element != 2:
+            return False
+    return True
+
+def stop_and_back():
+    fc.stop()
+    time.sleep(back_time)
+    fc.backward(back_speed)
+    time.sleep(back_time)
+    fc.stop()
 
 def main():
-    start_v = True
+    global on_stop
     while True:
-        scan_list = fc.scan_step(35)
+        # Distance of read - Threshold distance to avoid obstacles
+        scan_list = fc.scan_step(30)
         if not scan_list:
+            print("No scan")
             continue
 
-        tmp = scan_list[3:7]
-        print(tmp)
-#        if tmp != [2,2,2,2]:
-#            choice = random.randrange(2)
-#            print("choice: ", choice)
-#            if not start_v:
-#                fc.backward(back_speed)
-#                time.sleep(back_time)
-#            if choice == 0:
-#                fc.turn_right(speed)
-#            else:
-#                fc.turn_left(speed)
-#        else:
-#            fc.forward(speed)
+        #  Maximum amount of reads is 10
+        scan_read = scan_list[1:8]
+        read_size = len(scan_read)
+        print(scan_read)
+        print(read_size)
+        #if (read_size == 5 and tmp != [2,2,2,2,2]) or (read_size < 5 ):
+        if (not are_all_elements_equal_to_2(scan_read)):
+            choice = random.randrange(2)
+            print("choice: ", choice)
+            # if it is NOT first iteration. Go backwards when obstacle found.
+            # On first iteration is better to assess 
+            if not on_stop:
+                stop_and_back()
+                on_stop = True
+            else:
+                on_stop = False
+                if choice == 0:
+                    fc.turn_right(speed)
+                    print("Right")
+                else:
+                    fc.turn_left(speed)
+                    print("Left")
+        else:
+            on_stop = False
+            fc.forward(speed)
+            print("Forward")
 
-        start_v = False
 
 if __name__ == "__main__":
     try: 
